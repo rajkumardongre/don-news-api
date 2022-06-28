03ccdc7e62e5461642f03c7c7be51c91a41c023e
RAJKUMAR DONGRE <77038993+rajkumardongre@users.noreply.github.com>
2022-06-28 23:47:30 +0530

    add new features

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 // const path = require('path')
 const express = require("express");
+const https = require('https')
 
-const fetchWithKeyword = require(`${__dirname}/router/fetchWithKeyword.js`);
-const fetchTopHeadline = require(`${__dirname}/router/fetchTopHeadline.js`);
-const newsWithKeywordCountry = require(`${__dirname}/router/newsWithKeywordCountry.js`);
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -20,19 +18,92 @@ app.get("/api/v1", (req, res) => {
     const apiKey = query.apiKey;
     if (query.q) {
       const q = query.q;
-      fetchWithKeyword(apiKey, q, (data) => {
-        res.json(data);
+      const userAgent = req.get('user-agent');
+      const options = {
+          host: 'newsapi.org',
+          path : `/v2/everything?apiKey=${apiKey}&q=${q}`,
+          headers: {
+              'User-Agent': userAgent
+          }
+      }
+      https.get(options, function (response) {
+          let data;
+          response.on('data', function (chunk) {
+              if (!data) {
+                  data = chunk;
+              }
+              else {
+                  data += chunk;
+              }
+          });
+          response.on('end', function () {
+              const newsData = JSON.parse(data);
+              res.send(newsData)
+              // console.log(newsData);
+          });
       });
+      // fetchWithKeyword(apiKey, q, (data) => {
+      //   res.json(data);
+      // });
     } else if (query.country && query.category) {
-      newsWithKeywordCountry(apiKey, query.country, query.category, (data) => {
-        res.json(data);
+      const userAgent = req.get('user-agent');
+      const options = {
+          host: 'newsapi.org',
+          path: `/v2/top-headlines?country=${query.country}&apiKey=${apiKey}&category=${query.category}`,
+          headers: {
+              'User-Agent': userAgent
+          }
+      }
+      https.get(options, function (response) {
+          let data;
+          response.on('data', function (chunk) {
+              if (!data) {
+                  data = chunk;
+              }
+              else {
+                  data += chunk;
+              }
+          });
+          response.on('end', function () {
+              const newsData = JSON.parse(data);
+              res.send(newsData)
+              // console.log(newsData);
+          });
       });
+      // newsWithKeywordCountry(apiKey, query.country, query.category, (data) => {
+      //   res.json(data);
+      // });
     } else {
       // console.log(query);
       // res.json({ message: "In Progress" });
-      fetchTopHeadline(apiKey, (data) => {
-        res.json(data);
+      const userAgent = req.get('user-agent');
+      const options = {
+          host: 'newsapi.org',
+          path: `/v2/top-headlines?country=in&apiKey=${apiKey}`,
+          headers: {
+              'User-Agent': userAgent
+          }
+      }
+      https.get(options, function (response) {
+          let data;
+          response.on('data', function (chunk) {
+              if (!data) {
+                  data = chunk;
+              }
+              else {
+                  data += chunk;
+              }
+          });
+          response.on('end', function () {
+              const newsData = JSON.parse(data);
+              // console.log(newsData);
+              res.send(newsData)
+          });
       });
+      // res.send("hello");
+      // fetchTopHeadline(apiKey, (data) => {
+      //   res.json(data);
+      // });
     }
   } else {
     res.json({ error: "api key required" });
